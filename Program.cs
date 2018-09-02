712959a9836a67b5b0f0dd26d1b9fc6c94690e66
tawab_coder <abduttawabcse@live.com>
2018-09-03 00:44:09 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AesEncTest
+{
+    using System;
+    using System.IO;
+    using System.Security.Cryptography;
+
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+
+                string original = "Here is some data to encrypt!";
+
+                // Create a new instance of the Aes 
+                // class.  This generates a new key and initialization  
+                // vector (IV). 
+                using (var random = new RNGCryptoServiceProvider())
+                {
+                    var key = new byte[32];
+                     key = Encoding.UTF8.GetBytes("MySecretttttt1444454545454545454"); ;
+                    random.GetBytes(key);
+
+                    // Encrypt the string to an array of bytes. 
+                    byte[] encrypted = EncryptStringToBytes_Aes(original, key);
+                    var result = Convert.ToBase64String(encrypted);
+                    // Decrypt the bytes to a string. 
+                    string roundtrip = DecryptStringFromBytes_Aes(encrypted, key);
+
+                    //Display the original data and the decrypted data.
+                    Console.WriteLine("Original:   {0}", original);
+                    Console.WriteLine("Encrypted (b64-encode): {0}", result);
+                    Console.WriteLine("Round Trip: {0}", roundtrip);
+                    Console.ReadKey();
+                   var encResult= EncryptString("Hy this is Asif", "MySecretttttt1444454545454545454");
+                    var decRes = Decrypt(encResult, "MySecretttttt1444454545454545454");
+                    //  var decRes = Decrypt("cu0KE5q0EEBFk21zd3Izjw==", "MySecretttttt1444454545454545454");
+                    Console.WriteLine("New Dec: {0}", roundtrip);
+
+                    Console.ReadLine();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
+        }
+
+
+        public static string EncryptString(string message, string KeyString)
+        {
+            byte[] Key = ASCIIEncoding.UTF8.GetBytes(KeyString);
+            //  byte[] IV = ASCIIEncoding.UTF8.GetBytes(IVString);
+           
+            string encrypted = null;
+            RijndaelManaged rj = new RijndaelManaged();
+            byte[] IV = new byte[rj.IV.Length];
+            rj.Key = Key;
+            rj.IV = IV;
+            rj.Mode = CipherMode.CBC;
+
+            try
+            {
+                MemoryStream ms = new MemoryStream();
+
+                using (CryptoStream cs = new CryptoStream(ms, rj.CreateEncryptor(Key, IV), CryptoStreamMode.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(cs))
+                    {
+                        sw.Write(message);
+                        sw.Close();
+                    }
+                    cs.Close();
+                }
+                byte[] encoded = ms.ToArray();
+                encrypted = Convert.ToBase64String(encoded);
+
+                ms.Close();
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+                return null;
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine("A file error occurred: {0}", e.Message);
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: {0}", e.Message);
+            }
+            finally
+            {
+                rj.Clear();
+            }
+            return encrypted;
+        }
+
+        // Decrypt a byte array into a byte array using a key and an IV 
+        // Decrypt a string into a string using a key and an IV 
+        public static string Decrypt(string cipherData, string keyString)
+        {
+            byte[] key = Encoding.UTF8.GetBytes(keyString);
+          //  byte[] iv = Encoding.UTF8.GetBytes(ivString);
+            RijndaelManaged myRijndael = new RijndaelManaged();
+            byte[] iv = new byte[myRijndael.IV.Length];
+            try
+            {
+                using (var rijndaelManaged =
+                       new RijndaelManaged { Key = key, IV = iv, Mode = CipherMode.CBC })
+                using (var memoryStream =
+                       new MemoryStream(Convert.FromBase64String(cipherData)))
+                using (var cryptoStream =
+                       new CryptoStream(memoryStream,
+                           rijndaelManaged.CreateDecryptor(key, iv),
+                           CryptoStreamMode.Read))
+                {
+                    return new StreamReader(cryptoStream).ReadToEnd();
+                }
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+                return null;
+            }
+            // You may want to catch more exceptions here...
+        }
+        static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key)
+        {
+            byte[] encrypted;
+            byte[] IV;
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+
+                aesAlg.GenerateIV();
+                IV = aesAlg.IV;
+
+                aesAlg.Mode = CipherMode.CBC;
+
+                var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption. 
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            var combinedIvCt = new byte[IV.Length + encrypted.Length];
+            Array.Copy(IV, 0, combinedIvCt, 0, IV.Length);
+            Array.Copy(encrypted, 0, combinedIvCt, IV.Length, encrypted.Length);
+
+            // Return the encrypted bytes from the memory stream. 
+            return combinedIvCt;
+
+        }
+
+        static string DecryptStringFromBytes_Aes(byte[] cipherTextCombined, byte[] Key)
+        {
+
+            // Declare the string used to hold 
+            // the decrypted text. 
+            string plaintext = null;
+
+            // Create an Aes object 
+            // with the specified key and IV. 
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+
+                byte[] IV = new byte[aesAlg.BlockSize / 8];
+                byte[] cipherText = new byte[cipherTextCombined.Length - IV.Length];
+
+                Array.Copy(cipherTextCombined, IV, IV.Length);
+                Array.Copy(cipherTextCombined, IV.Length, cipherText, 0, cipherText.Length);
+
+                aesAlg.IV = IV;
+
+                aesAlg.Mode = CipherMode.CBC;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption. 
+                using (var msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+
+        }
+    }
+}
